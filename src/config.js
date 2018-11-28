d4c78f60b20e9d5498b508d7ff70de4d7024b764
Richard Duan <dxloader@gmail.com>
2018-11-28 22:01:18 +1100

    config amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "notes-app-uploads-ph"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://zzmtnlmwjk.execute-api.ap-southeast-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_SCLaM3fGQ",
+    APP_CLIENT_ID: "6hu418k8iui5vk02v547aqed0b",
+    IDENTITY_POOL_ID: "ap-southeast-1:ca49c4f6-5a63-4511-a1b6-c85f2a83e2c0"
+  }
+};
