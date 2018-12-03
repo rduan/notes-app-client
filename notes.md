e6277aec0e82fa3c296c847bd54fdf91a49df5ab
Richard Duan <dxloader@gmail.com>
2018-12-03 20:48:26 +1100

    configure environment

--- notes.md
+++ notes.md
@@ -3,3 +3,6 @@ aws s3 sync build/ s3://notes-app-client-ph --delete
 aws cloudfront create-invalidation --distribution-id YOUR_CF_DISTRIBUTION_ID --paths "/*"
 
 aws cloudfront create-invalidation --distribution-id EPZGAQ3RQ2PUY --paths "/*"
+
+
+REACT_APP_STAGE=prod npm run build
--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "ap-southeast-1",
     BUCKET: "notes-app-uploads-ph"
@@ -14,4 +13,28 @@ export default {
     APP_CLIENT_ID: "6hu418k8iui5vk02v547aqed0b",
     IDENTITY_POOL_ID: "ap-southeast-1:ca49c4f6-5a63-4511-a1b6-c85f2a83e2c0"
   }
+}
+
+const prod = {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "notes-app-uploads-ph"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://zzmtnlmwjk.execute-api.ap-southeast-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_SCLaM3fGQ",
+    APP_CLIENT_ID: "6hu418k8iui5vk02v547aqed0b",
+    IDENTITY_POOL_ID: "ap-southeast-1:ca49c4f6-5a63-4511-a1b6-c85f2a83e2c0"
+  }
+}
+
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
